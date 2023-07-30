c4c4cb892598f358abfb84ad0d732ca03a665441
Андрей <andrewerm@yandex.ru>
2023-07-30 18:54:26 +0300

    init

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,20 @@
+import {Outlet} from "react-router-dom";
+import {Layout} from 'antd';
+import React from "react";
+import {App as AppAnt} from 'antd';
+
+export const AppLayout: React.FC = () => {
+    const layoutStyle: React.CSSProperties = {
+        // height: '100vh'
+        minHeight: '100vh'
+    }
+    return (
+        <>
+            <AppAnt>
+                <Layout style={layoutStyle}>
+                    <Outlet/>
+                </Layout>
+            </AppAnt>
+        </>
+    )
+}
