a76d89c3f05447059013588c5fc93f4011730973
Андрей <andrewerm@yandex.ru>
2023-08-09 22:20:55 +0300

    workflow

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -3,5 +3,5 @@ import MockAdapter from 'axios-mock-adapter'
 
 const mock = new MockAdapter(axios, {delayResponse: 2000})
 
-if (process.env.NODE_ENV === 'development') axios.defaults.baseURL = 'https://91.201.53.108'
+if (process.env.NODE_ENV === 'development') axios.defaults.baseURL = 'https://85.12.225.131'
 export default mock
