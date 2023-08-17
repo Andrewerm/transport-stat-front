94c0a49af034ddc733268eac59091447916bdb5d
Андрей <andrewerm@yandex.ru>
2023-08-17 23:13:21 +0300

    адрес сервера

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -3,5 +3,6 @@ import MockAdapter from 'axios-mock-adapter'
 
 const mock = new MockAdapter(axios, {delayResponse: 2000})
 
-if (process.env.NODE_ENV === 'development') axios.defaults.baseURL = 'https://count.stellar-tb.ru'
+// if (process.env.NODE_ENV === 'development')
+    axios.defaults.baseURL = 'https://count.stellar-tb.ru'
 export default mock
