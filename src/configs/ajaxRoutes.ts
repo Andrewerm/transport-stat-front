91aad3985cf737027c1aa273606af1e66db865d0
Андрей <andrewerm@yandex.ru>
2023-08-19 09:56:42 +0300

    адрес сервера

--- src/configs/ajaxRoutes.ts
+++ src/configs/ajaxRoutes.ts
@@ -2,8 +2,8 @@ export enum AjaxRoutes {
     LOGIN='/auth/login',
     ROUTE_LOGIN='/sign/login',
     LOGOUT='/auth/logout',
-    ROUTE_REMIND_PASSWORD='/sign/remind',
-    POST_REMIND_PASSWORD='/auth/remind_password',
+    // ROUTE_REMIND_PASSWORD='/sign/remind',
+    // POST_REMIND_PASSWORD='/auth/remind_password',
     HOME='/',
     GET_VEHICLES='/guides/vehicles',
     POST_REPORT='/guides/report'
